082c96666a7f5f00d4be609c5e1753682891dfca
Mandar Patil <mandaarp@gmail.com>
2012-12-26 21:21:22 -0800

    deleted <6 kb files

--- code/rename_files.py
+++ code/rename_files.py
@@ -1,12 +1,11 @@
-PREFIX = "pedestrian-back-"
+import glob, os, sys
 
-import glob
-import os
-
-g = glob.glob("*.jpg")
-
-for i in range(len(g)):
-	os.rename(g[i], PREFIX + str(i+1) + ".jpg")
-
-
-
+if len(sys.argv) < 3:
+    print "usage: python " + sys.argv[0] + " PREFIX PATH"
+    exit
+else:
+    rename(sys.argv[2], sys.argv[1])
+def rename(path, prefix):
+    g = glob.glob(os.path.join(path,"*.jpg")
+    for i in range(len(g)):
+        os.rename(g[i], prefix + str(i+1) + ".jpg")
