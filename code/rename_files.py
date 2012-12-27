2379ad7543b491b2a5dc508a12a456ce009a0e7a
Mandar Patil <mandaarp@gmail.com>
2012-12-26 22:46:57 -0800

    renamed the files

--- code/rename_files.py
+++ code/rename_files.py
@@ -2,10 +2,10 @@ import glob, os, sys
 
 if len(sys.argv) < 3:
     print "usage: python " + sys.argv[0] + " PREFIX PATH"
-    exit
+    exit(1)
 else:
     rename(sys.argv[2], sys.argv[1])
 def rename(path, prefix):
     g = glob.glob(os.path.join(path,"*.jpg")
     for i in range(len(g)):
-        os.rename(g[i], prefix + str(i+1) + ".jpg")
+        os.rename(g[i], os.path.join(path,prefix + str(i+1) + ".jpg"))
